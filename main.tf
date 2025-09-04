4c32d9c6b9a33bd83bccebd5e2701a87226ec32f
chnaveen-devops <chinninaveen000@gmail.com>
2025-09-04 13:01:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,6 @@ resource "aws_instance" "one" {
     Name = var.instance_names[count.index]
   }
 }
-
 variable "instance_names" {
   default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
 }
